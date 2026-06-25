8da0115125bcf924c4676cfe1c91db8ac1e4ea10
unknown <tongnguyennhattien140805@gmail.com>
2026-06-25 22:22:37 +0700

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,11 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { beVNPro } from "@/utils";
+import { Geist } from "next/font/google";
 
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -26,9 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", geistMono.variable)}>
+    <html lang="en" className={cn(geistMono.variable, "font-sans", geist.variable)}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${beVNPro.variable}`}
       >
         {children}
       </body>
---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+import { Be_Vietnam_Pro } from "next/font/google";
+
+export const beVNPro = Be_Vietnam_Pro({
+    subsets: ["latin"],
+    variable: "--font-be-vietnam-pro",
+    weight: ["400", "500", "600", "700"],
+});
